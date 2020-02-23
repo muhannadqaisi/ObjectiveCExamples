138838cd19961d85f7246c0c40a3430b26f747cd
MuhannadQaisi <muhannadqaisi@yahoo.com>
2020-02-23 10:55:52 -0800

    Create Calc.h

---
+++ Calc/CalculatorObjC/Calc.h
@@ -0,0 +1,17 @@
+//
+//  Calc.h
+//  CalculatorObjC
+//
+//  Created by Muhannad Qaisi on 2/23/20.
+//  Copyright © 2020 Muhannad Qaisi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@protocol Calc <NSObject>
+
+@end
+
+NS_ASSUME_NONNULL_END
