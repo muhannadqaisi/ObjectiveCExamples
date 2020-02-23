28827ee14adbba662b06a64a0ee224050e73b6e5
MuhannadQaisi <muhannadqaisi@yahoo.com>
2020-02-23 10:53:27 -0800

    Create Animal+Exam.h

---
+++ Toturial/Objective-C-tut/Animal+Exam.h
@@ -0,0 +1,20 @@
+//
+//  Animal+Exam.h
+//  Objective-C-tut
+//
+//  Created by Muhannad Qaisi on 11/25/19.
+//  Copyright © 2019 Muhannad Qaisi. All rights reserved.
+//
+
+#import "Animal.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Animal (Exam)
+
+-(BOOL)checkedByVet;
+-(void)getShots;
+
+@end
+
+NS_ASSUME_NONNULL_END
